a22f37f90a992c86c51e3717038a3aab25dab17f
mufu <mufu@bantouyan.com>
2023-08-22 16:29:59 +0800

    添加CI及Dockerfile

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from db.database import Base, engine
 from api.v1.endpoints import images, prompts, sd_vars, image_tasks
 from fastapi.middleware.cors import CORSMiddleware
+from os import environ
+import uvicorn
 
 
 Base.metadata.create_all(bind=engine)  # 创建数据库表
@@ -11,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://192.168.0.10:3000"],  # React 默认端口
+    allow_origins=["*"],  # React 默认端口
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,3 +24,10 @@ app.include_router(images.router)
 app.include_router(prompts.router)
 app.include_router(sd_vars.router)
 app.include_router(image_tasks.router)
+
+
+if __name__ == "__main__":
+    host = "0.0.0.0"
+    port = environ["SERVER_PORT"]
+    print(f"Running on http://{host}:{port}")
+    uvicorn.run("main:app", host=host, port=int(port), reload=True)
